717d7c0db0221e9ef9b974a1779a2e6490b6bd01
Healthpy <mexichem25@gmail.com>
2023-05-24 12:13:40 +0300

    update

---
+++ dataset.py
@@ -0,0 +1,70 @@
+import os
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
+import wfdb
+
+
+def scaling(X, sigma=0.1):
+    scalingFactor = np.random.normal(loc=1.0, scale=sigma, size=(1, X.shape[1]))
+    myNoise = np.matmul(np.ones((X.shape[0], 1)), scalingFactor)
+    return X * myNoise
+
+
+def shift(sig, interval=20):
+    for col in range(sig.shape[1]):
+        offset = np.random.choice(range(-interval, interval))
+        sig[:, col] += offset / 1000 
+    return sig
+
+
+def transform(sig, train=False):
+    if train:
+        if np.random.randn() > 0.5: sig = scaling(sig)
+        if np.random.randn() > 0.5: sig = shift(sig)
+    return sig
+
+
+class ECGDataset(Dataset):
+    def __init__(self, phase, data_dir, hrv_features, label_csv, folds, leads):
+        super(ECGDataset, self).__init__()
+        self.phase = phase
+        df = pd.read_csv(label_csv)
+        df = df[df['fold'].isin(folds)]
+        self.data_dir = data_dir
+        df_hrv = pd.read_csv(hrv_features)
+        df_hrv.replace([np.inf, -np.inf], 100000, inplace=True)
+        df_hrv = df_hrv.merge(df[['patient_id', 'fold']], on='patient_id')
+        self.hrv_features = df_hrv[df_hrv.columns[1:-1]].to_numpy()
+        
+        self.labels = df
+        self.leads = ['I', 'II', 'III', 'aVR', 'aVL', 'aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']
+        if leads == 'all':
+            self.use_leads = np.where(np.in1d(self.leads, self.leads))[0]
+        else:
+            self.use_leads = np.where(np.in1d(self.leads, leads))[0]
+        self.nleads = len(self.use_leads)
+        self.classes = ['SNR', 'AF', 'IAVB', 'LBBB', 'RBBB', 'PAC', 'PVC', 'STD', 'STE']
+        self.n_classes = len(self.classes)
+        self.data_dict = {}
+        self.label_dict = {}
+
+    def __getitem__(self, index: int):
+        row = self.labels.iloc[index]
+        patient_id = row['patient_id']
+        ecg_data, _ = wfdb.rdsamp(os.path.join(self.data_dir, patient_id))
+        ecg_data = transform(ecg_data, self.phase == 'train')
+        nsteps, _ = ecg_data.shape
+        ecg_data = ecg_data[-15000:, self.use_leads]
+        result = np.zeros((15000, self.nleads)) # 30 s, 500 Hz
+        result[-nsteps:, :] = ecg_data
+        if self.label_dict.get(patient_id):
+            labels = self.label_dict.get(patient_id)
+        else:
+            labels = row[self.classes].to_numpy(dtype=np.float32)
+            self.label_dict[patient_id] = labels
+        return torch.from_numpy(result.transpose()).float(), torch.from_numpy(self.hrv_features[index]).float(), torch.from_numpy(labels).float()
+
+    def __len__(self):
+        return len(self.labels)
---
+++ utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, roc_auc_score
+import wfdb
+
+
+def split_data(seed=42):
+    folds = range(1, 11)
+    folds = np.random.RandomState(seed).permutation(folds)
+    return folds[:8], folds[8:9], folds[9:]
+
+
+def prepare_input(ecg_file: str):
+    if ecg_file.endswith('.mat'):
+        ecg_file = ecg_file[:-4]
+    ecg_data, _ = wfdb.rdsamp(ecg_file)
+    nsteps, nleads = ecg_data.shape
+    ecg_data = ecg_data[-15000:, :]
+    result = np.zeros((15000, nleads)) # 30 s, 500 Hz
+    result[-nsteps:, :] = ecg_data
+    return result.transpose()
+
+def cal_scores(y_true, y_pred, y_score):
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    auc = roc_auc_score(y_true, y_score)
+    acc = accuracy_score(y_true, y_pred)
+    return precision, recall, f1, auc, acc
+
+
+def find_optimal_threshold(y_true, y_score):
+    thresholds = np.linspace(0, 1, 100)
+    f1s = [f1_score(y_true, y_score > threshold) for threshold in thresholds]
+    return thresholds[np.argmax(f1s)]
+
+
+def cal_f1(y_true, y_score, find_optimal):
+    if find_optimal:
+        thresholds = np.linspace(0, 1, 100)    
+    else:
+        thresholds = [0.5]
+    f1s = [f1_score(y_true, y_score > threshold) for threshold in thresholds]
+    return np.max(f1s)
+
+
+def cal_f1s(y_trues, y_scores, find_optimal=True):
+    f1s = []
+    for i in range(y_trues.shape[1]):
+        f1 = cal_f1(y_trues[:, i], y_scores[:, i], find_optimal)
+        f1s.append(f1)
+    return np.array(f1s)
+
+
+def cal_aucs(y_trues, y_scores):
+    return roc_auc_score(y_trues, y_scores, average=None)
